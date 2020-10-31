6d5bd8e1c265d092a7b3cf2d1e4f4d4bce05c7f4
Константин Спицин <k.spitsin@bars.group>
2020-10-31 16:26:41 +0300

    added second(first) part in sevens lab and added draw dunc a many plots

--- Graph2/Library.fs
+++ Graph2/Library.fs
@@ -9,3 +9,13 @@ module Graph =
     
     let draw3 r1 r2 r3 r4 r5 r6 = [Scatter (x = r1, y = r2);  Scatter (x = r3, y = r4); Scatter (x = r5, y = r6)] |> Chart.Plot |> Chart.WithWidth 700 |> Chart.WithHeight 500 |> Chart.Show
     
+    let drawMany xss yss =
+        let scatters = (xss, yss) ||> List.map2 (fun xs ys -> Scatter (x = xs, y = ys))
+        
+        scatters
+        |> Chart.Plot
+        |> Chart.WithWidth 700
+        |> Chart.WithHeight 500
+        |> Chart.Show
+    
+    
